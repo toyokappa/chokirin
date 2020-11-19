9b472b77eca8ad9227468b3283102e4ec94a1fbf
Yuta Toyokawa <kppg42@gmail.com>
2020-11-19 20:32:47 +0900

    モーダル処理の実装

---
+++ components/modules/Modal.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+transition(name="modal", appear)
+  .modal-overlay(@click="closeModal")
+    slot
+</template>
+
+<script>
+export default {
+  methods: {
+    closeModal(event) {
+      if (event.target.classList.contains("modal-overlay")) {
+        this.$emit("closeModal");
+      }
+    },
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.modal-overlay
+  display: flex
+  align-items: center
+  justify-content: center
+  position: fixed
+  z-index: 1050
+  top: 0
+  left: 0
+  height: 100%
+  width: 100%
+  background-color: rgba(0, 0, 0, 0.7)
+
+// transition
+.modal-enter-active, .modal-leave-active
+  transition: opacity 0.3s
+  &> *
+    transition: opacity 0.3s, transform 0.4s
+.modal-leave-active
+  transition: opacity 0.6s ease 0.4s
+.modal-enter, .modal-leave-to
+  opacity: 0
+  &> *
+    opacity: 0
+    transform: translateY(-20px)
+</style>
--- components/sections/PhotoTile.vue
+++ components/sections/PhotoTile.vue
@@ -3,7 +3,7 @@
   .container
     .row.row-3
       .col-sm-3.col-4.mb-3
-        .photo
+        .photo(@click="openModal()")
       .col-sm-3.col-4.mb-3
         .photo
       .col-sm-3.col-4.mb-3
@@ -26,7 +26,42 @@
         .photo
       .col-sm-3.col-4.mb-3
         .photo
+  m-modal(v-if="modalFlag", @closeModal="closeModal")
+    .work-detail
+      .work-photo
+      .work-content
+        .work-name
+          | みゆさん
+        .work-profile
+          | 20代後半女性（主婦）
+        .work-comment
+          | 今日も縮毛矯正をしてもらいました。通いはじめてもうじき2年になりますが、毎回丁寧にしてくれます（＾ω＾）仕上がりも満足です。髪の量が多く癖なのですが、持ちも良いです。ありがとうございました！
+        .work-date
+          | 2018.09.14
 </template>
+
+<script>
+import MModal from "@/components/modules/Modal";
+
+export default {
+  components: {
+    MModal,
+  },
+  data() {
+    return {
+      modalFlag: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.modalFlag = true;
+    },
+    closeModal() {
+      this.modalFlag = false;
+    },
+  },
+};
+</script>
 
 <style lang="sass" scoped>
 .photo-tile
@@ -34,6 +69,39 @@
     width: 100%
     padding-bottom: 100%
     background-color: #c4c4c4
+    cursor: pointer
+  .work-detail
+    width: 100%
+    max-width: 800px
+    height: 400px
+    display: flex
+    .work-photo
+      width: 100%
+      max-width: 400px
+      background-color: #c4c4c4
+    .work-content
+      width: 100%
+      max-width: 400px
+      background-color: white
+      padding: 20px
+      position: relative
+      .work-name
+        font-size: 20px
+        font-weight: bold
+        text-align: center
+        margin-bottom: 10px
+      .work-profile
+        font-size: 16px
+        text-align: center
+        margin-bottom: 30px
+      .work-comment
+        font-size: 16px
+        line-height: 1.8
+      .work-date
+        font-size: 14px
+        position: absolute
+        right: 20px
+        bottom: 20px
 .row-3
   margin-left: -0.5rem
   margin-right: -0.5rem
